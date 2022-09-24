3557e05b6db9340a49b5a5b4df2a9e1cad996dbd
lelio <lelio@pop-os.localdomain>
2022-09-24 18:06:08 -0300

    public

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 require('dotenv').config();
 
+app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
 
